7c6aaa799f191c11f362a42c77ad1b59c8f8d0b5
Bill Lou <billloukehao@gmail.com>
2019-05-16 14:26:46 -0700

    Setup eslint and some node modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,36 @@
+module.exports = {
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  rules: {
+    'no-console': 'warn',
+    'comma-dangle': 'warn',
+    "prefer-const": 'warn',
+    'no-unused-vars': [
+      'warn',
+      {
+        'vars': 'local',
+        'args': 'none'
+      }
+    ]
+  },
+  "env": {
+    "node": true,
+    "browser": true,
+    "es6": true
+  }
+}
+
+
 /**
  * These rules enforce the Hack Reactor Style Guide
  *
  * Visit this repo for more information:
  *   https://github.com/reactorcore/eslint-config-hackreactor
- */
+
 
 module.exports = {
   extends: './node_modules/eslint-config-hackreactor/index.js'
 };
+*/
