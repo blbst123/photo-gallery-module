ce593c4c98f283fb03b49ba6b4727e3f310236d4
Bill Lou <billloukehao@gmail.com>
2019-05-17 14:42:08 -0700

    Implement fake data seeding script
    - Uses node module faker
    - ./database/generateData.js export the functions which will generate data
    - In root folder of the project, run "npm run generate-data" in terminal after setting up a database called 'photo-gallery'

---
+++ database/generateData.js
@@ -0,0 +1,63 @@
+/* eslint-disable func-names */
+// Create tables: Images, Users, Restaurants
+const faker = require('faker');
+
+const getProfileUrl = function () {
+  const index = Math.floor(Math.random() * 10);
+  return `https://s3.us-east-2.amazonaws.com/team419photo-gallery-users/users${index}.jpeg`;
+};
+
+const getImageUrl = function () {
+  const index = Math.floor(Math.random() * 30);
+  return `https://s3.us-east-2.amazonaws.com/team419photo-gallery/foodImages${index}.jpg`;
+};
+
+module.exports.generateUsers = function (num = 100) {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push({
+      id: i,
+      name: faker.name.findName(),
+      rating: Math.floor(Math.random() * 5 + 1),
+      friends: Math.floor(Math.random() * 100),
+      reviews: Math.floor(Math.random() * 100),
+      profile_url: getProfileUrl()
+    });
+  }
+
+  return arr;
+};
+
+module.exports.generateRestaurants = function (num = 100) {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push({
+      id: i,
+      name: faker.company.companyName(),
+      photos: Math.floor(Math.random() * 20)
+    });
+  }
+
+  return arr;
+};
+
+module.exports.generateImages = function (num = 1000) {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push({
+      id: i,
+      image_url: getImageUrl(),
+      user_id: Math.floor(Math.random() * 100),
+      comment: faker.lorem.sentence(),
+      date: faker.date.recent(),
+      helpful: Math.floor(Math.random() * 20),
+      not_helpful: Math.floor(Math.random() * 20),
+      restaurant_id: Math.floor(Math.random() * 10)
+    });
+  }
+
+  return arr;
+};
