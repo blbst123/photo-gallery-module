b41fa70b68797d3bbb40bb5d823ba58539b79ffb
Bill Lou <billloukehao@gmail.com>
2019-05-28 12:19:24 -0700

    Refactor to add clickevent to window

--- client/src/components/PhotoModal.js
+++ client/src/components/PhotoModal.js
@@ -5,48 +5,68 @@ import PhotoModalImage from './PhotoModalImage';
 import PhotoModalComment from './PhotoModalComment';
 import PhotoModalAd from './PhotoModalAd';
 
+<<<<<<< HEAD
 const PhotoModal = function (props) {
   const { hideModal, photo, changePhoto } = props;
+=======
+export default class PhotoModal extends React.Component {
+  constructor(props) {
+    super(props);
+>>>>>>> Refactor to add clickevent to window
 
-  const image = {
-    url: photo.modalPhoto.image_url,
-    photoNum: photo.photoNum,
-    photoTotalNum: photo.photoTotalNum
-  };
-  const comment = {
-    text: photo.modalPhoto.comment,
-    date: photo.modalPhoto.date
-  };
-  const { modalUser } = photo;
+    this.handleKey = this.handleKey.bind(this);
+  }
 
-  const handleKey = function (e) {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKey);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKey (e) {
     console.log('herere');
-    console.log(e.keycode);
+    console.log(e.key);
   };
 
+  render() {
+    const { visibility, hideModal, photo, changePhoto } = this.props;
+    const visibilityStyle = {
+      visibility
+    };
 
-  return (
-    <div className="photo-modal-bg" style={visibilityStyle} onClick={hideModal} tabIndex="0" onKeyDown={handleKey}>
-      <div className="photo-modal" onClick={(e) => { e.stopPropagation(); }}>
-        <div className="photo-modal-close" onClick={hideModal}>
-          <p>Close</p>
-          <svg height="100%" viewBox="0 0 24 24" width="100%">
-            <path d="M17.657 19.07L12 13.415 6.343 19.07 4.93 17.658 10.585 12 4.93 6.343 6.342 4.93 12 10.585l5.657-5.657L19.07 6.34 13.416 12l5.657 5.657-1.413 1.414z" />
-          </svg>
+    const image = {
+      url: photo.modalPhoto.image_url,
+      photoNum: photo.photoNum,
+      photoTotalNum: photo.photoTotalNum
+    };
+    const comment = {
+      text: photo.modalPhoto.comment,
+      date: photo.modalPhoto.date
+    };
+    const { modalUser } = photo;
+
+    return (
+      <div className="photo-modal-bg" style={visibilityStyle} onClick={hideModal} tabIndex="0" onKeyDown={this.handleKey}>
+        <div className="photo-modal" onClick={(e) => { e.stopPropagation(); }}>
+          <div className="photo-modal-close" onClick={hideModal}>
+            <p>Close</p>
+            <svg height="100%" viewBox="0 0 24 24" width="100%">
+              <path d="M17.657 19.07L12 13.415 6.343 19.07 4.93 17.658 10.585 12 4.93 6.343 6.342 4.93 12 10.585l5.657-5.657L19.07 6.34 13.416 12l5.657 5.657-1.413 1.414z" />
+            </svg>
+          </div>
+          <PhotoModalImage image={image} changePhoto={changePhoto} />
+          <PhotoModalComment user={modalUser} comment={comment} />
+          <PhotoModalAd />
         </div>
-        <PhotoModalImage image={image} changePhoto={changePhoto} />
-        <PhotoModalComment user={modalUser} comment={comment} />
-        <PhotoModalAd />
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 
 PhotoModal.propTypes = {
   hideModal: PropTypes.func.isRequired,
   changePhoto: PropTypes.func.isRequired,
   photo: PropTypes.object.isRequired
 };
-
-export default PhotoModal;
