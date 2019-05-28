bcdaf7b8cd621e053927e1f3aa2e26eb2361d829
Bill Lou <billloukehao@gmail.com>
2019-05-27 19:00:56 -0700

    Implement close button for PhotoModal

--- client/src/components/PhotoModal.js
+++ client/src/components/PhotoModal.js
@@ -22,6 +22,12 @@ const PhotoModal = function (props) {
   return (
     <div className="photo-modal-bg" onClick={hideModal}>
       <div className="photo-modal" onClick={(e) => { e.stopPropagation(); }}>
+        <div className="photo-modal-close" onClick={hideModal}>
+          <p>Close</p>
+          <svg height="100%" viewBox="0 0 24 24" width="100%">
+            <path d="M17.657 19.07L12 13.415 6.343 19.07 4.93 17.658 10.585 12 4.93 6.343 6.342 4.93 12 10.585l5.657-5.657L19.07 6.34 13.416 12l5.657 5.657-1.413 1.414z" />
+          </svg>
+        </div>
         <PhotoModalImage image={image} changePhoto={changePhoto} />
         <PhotoModalComment user={modalUser} comment={comment} />
         <PhotoModalAd />
